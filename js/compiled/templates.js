6bacf0e007d75a8487aec6e0035e42a98d994126
Philip Harrison <philip@mailharrison.com>
2011-10-28 20:22:13 +0200

    delete button

--- js/compiled/templates.js
+++ js/compiled/templates.js
@@ -79,6 +79,6 @@ templates['row'] = template(function (Handlebars,depth0,helpers,partials,data) {
   stack1 = helpers.body || depth0.body
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "body", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "</p>\n</div>\n<div class=\"box center arrow\">A</div>";
+  buffer += escapeExpression(stack1) + "</p>\n</div>\n<div class=\"box center arrow\">\n  <span>A</span>\n  <button class=\"delete delete_m\" style=\"display:none\">Delete</button>\n</div>";
   return buffer;});
 })()
