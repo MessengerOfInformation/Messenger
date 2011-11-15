c9a4babc7edd52c4d2a0f7d7806cf101a74bec69
Philip Harrison <philip@mailharrison.com>
2011-11-15 22:55:08 +0100

    old

--- scripts/compileTemplates.js
+++
@@ -1,6 +0,0 @@
-var exec   = require('child_process').exec
-var sys    = require('sys')
-child = exec('handlebars  js/templates/* -f js/compiled/templates.js')
-child.stdout.on('data', function(data) { 
-  sys.print(data) 
-})
